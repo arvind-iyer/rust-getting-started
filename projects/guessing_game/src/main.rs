6b07eb8078da308a87e2c95a9d1b5a6a33488cd8
Arvind Iyer <mail@arvindiyer.xyz>
2016-06-10 06:11:08 +0800

    match, types, , loops, IO and error handling

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,16 +1,43 @@
+extern crate rand;
+
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number");
-    println!("Please input your guess: ");
-    //Define a mutable string 
-    let mut guess =  String::new();
     
-    //Grab input and store in guess and handle error with appropriate message
-    //Pass mutable reference to guess explicitly as rust references are immutable by default
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    // Generate a random number between 1 and 100
+    let secret_num = rand::thread_rng().gen_range(1,101);
+
     
-    println!("You guessed : {} ", guess);
+    loop {
+        println!("Please input your guess: ");
+        //Define a mutable string 
+        let mut guess =  String::new();
+    
+        //Grab input and store in guess and handle error with appropriate message
+        //Pass mutable reference to guess explicitly as rust references are immutable by default
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        //convert guess to an integer
+        //if input is not an integer, ignore and ask for input again
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed : {} ", guess);
+
+        match guess.cmp(&secret_num) {
+            Ordering::Less      => println!("Too small"),
+            Ordering::Greater   => println!("Too big"),
+            Ordering::Equal     => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
  
